94eeb6652b45252fafa3c84e53a6f1579b477589
Jhon E Villarreal P <jhon_villarreal@epam.com>
2024-06-12 19:44:08 -0500

    feature: Adding directory service

---
+++ src/core/services/directory-service.js
@@ -0,0 +1,23 @@
+class DirectoryService {
+  constructor(directoryRepository) {
+    throw new Error("To be implemented");
+  }
+
+  create(path) {
+    throw new Error("To be implemented");
+  }
+
+  move(source, destination) {
+    throw new Error("To be implemented");
+  }
+
+  delete(path) {
+    throw new Error("To be implemented");
+  }
+
+  list() {
+    throw new Error("To be implemented");
+  }
+}
+
+module.exports = DirectoryService;
