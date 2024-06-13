4fa52ea09a6f59597dc4de59519ffe1a1fba7c4e
Jhon E Villarreal P <jhon_villarreal@epam.com>
2024-06-12 19:40:30 -0500

    feature: Adding directory repository interface

---
+++ src/core/ports/output/directory-repository.js
@@ -0,0 +1,19 @@
+class DirectoryRepository {
+  create(path) {
+    throw new Error("To be implemented");
+  }
+
+  move(source, destination) {
+    throw new Error("To be implemented");
+  }
+
+  delete(path) {
+    throw new Error("To be implemented");
+  }
+
+  list() {
+    throw new Error("To be implemented");
+  }
+}
+
+module.exports = DirectoryRepository;
