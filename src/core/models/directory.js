e7a4170cca8457d45f97d10b5cdab3daa1c928a3
Jhon E Villarreal P <jhon_villarreal@epam.com>
2024-06-12 20:26:09 -0500

    feature: Adding create and list command on model and its tests

--- src/core/models/directory.js
+++ src/core/models/directory.js
@@ -5,7 +5,14 @@ class Directory {
   }
 
   create(path) {
-    throw new Error("To be implemented");
+    const parts = path.split("/");
+    let current = this;
+    for (const part of parts) {
+      if (!current.subdirectories.has(part)) {
+        current.subdirectories.set(part, new Directory(part));
+      }
+      current = current.subdirectories.get(part);
+    }
   }
 
   move(source, destination) {
@@ -17,7 +24,12 @@ class Directory {
   }
 
   list(indent = 0) {
-    throw new Error("To be implemented");
+    const spaces = " ".repeat(indent * 2);
+    let result = `${spaces}${this.name}\n`;
+    for (const [name, subDir] of this.subdirectories) {
+      result += subDir.list(indent + 1);
+    }
+    return result;
   }
 }
 
